bde358a8a1fcf79f4c0f40f48ecddf72209eca15
AlessandroSigna <alesigna92@gmail.com>
2018-07-05 13:38:25 +0200

    setup assets e sdk

---
+++ Assets/Scenes/ScriptTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptTest : MonoBehaviour {
+    
+
+	void Update() {
+        transform.localRotation = Camera.main.transform.rotation;
+	}
+    
+}
