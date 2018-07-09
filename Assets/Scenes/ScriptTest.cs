aa5bbd470f2da09ec3c94b124b54b288df871305
EdgarPironti <pacepiro@hotmail.it>
2018-07-09 20:56:41 +0200

    rotation fix, new models, material refining

--- Assets/Scenes/ScriptTest.cs
+++ Assets/Scenes/ScriptTest.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ScriptTest : MonoBehaviour {
-    
+	public float distanceFromCamera;
+	public float height;
 
 	void Update() {
-        transform.localRotation = Camera.main.transform.rotation;
+        //transform.localRotation = Camera.main.transform.rotation;
+		//transform.position = (Camera.main.transform.forward * distanceFromCamera);
+		transform.rotation = Quaternion.identity;
 	}
     
 }
